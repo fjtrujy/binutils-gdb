8f2a0502970b12fc7e6b18bce9b83e9993c5b763
Francisco Javier Trujillo Mata <fjtrujy@gmail.com>
2024-05-04 15:31:13 +0200

    Needed fixes

--- gas/write.c
+++ gas/write.c
@@ -1020,7 +1020,7 @@ fixup_segment (fixS *fixP, segT this_segment)
 	}
 
       if (!fixP->fx_done)
-	md_apply_fix (fixP, &add_number, this_segment);
+	md_apply_fix3 (fixP, &add_number, this_segment);
 
       if (!fixP->fx_done)
 	{
